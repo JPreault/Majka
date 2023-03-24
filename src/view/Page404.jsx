802fa699f8e1c9c77c094abefc8be6cd5d5e9bf2
José <jose@preault.com>
2023-03-24 01:30:42 +0100

    feat(front-end): ajout de la page 404

---
+++ src/view/Page404.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import NotFoundIcon from '../images/jpg/404.png';
+
+function Page404 () {
+    const navigate = useNavigate();
+
+    return (<div className='container__notfound'>
+        <img src={NotFoundIcon} alt={'404'}></img>
+        <p>La page que vous cherchez semble introuvable</p>
+        <p onClick={() => navigate('/')} className='link clickable'>Retour vers la page d&apos;accueil</p>
+    </div>);
+}
+
+export default Page404;
