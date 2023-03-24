b05fabe8fadc4e18682188c332b8d80e8618ff6e
José <jose@preault.com>
2023-03-24 01:26:58 +0100

    chore: démarrage du projet sur de bonnes bases

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback } from 'react';
+import { Outlet } from 'react-router-dom';
+import useOnResize from './hooks/useOnResize';
+import Menu from './components/Menu';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+    const updateHeight = useCallback(() => {
+        const vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--doc-height', `${vh}px`);
+    }, []);
+
+    useOnResize(() => {
+        updateHeight();
+    });
+
+    updateHeight();
+
+    return (
+        <div className="App">
+            <Menu/>
+            <Outlet/>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import Login from './view/Homepage';
+import Page404 from './view/Page404';
+import './sass/main.scss';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
+
+ReactDOM.render(
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<App />}>
+                        <Route index element={<Login page='login'/>} />
+                        <Route path='game1' element={<Login page='forgotPassword'/>} />
+                        <Route path='game2' element={<Login page='mobileLogin'/>} />
+                        <Route path='game3' element={<Login page='newPassword'/>} />
+                        <Route path='game4' element={<Login page='newPassword'/>} />
+                        <Route path='config' element={<Login page='newPassword'/>} />
+                        <Route path='finish' element={<Login page='newPassword'/>} />
+                        <Route path="*"element={<Page404/>} />
+                    </Route>
+                </Routes>
+            </Router>
+        </QueryClientProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/* <ReactQueryDevtools initialIsOpen={false} /> */
