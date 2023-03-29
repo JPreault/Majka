a5304ae12344365b63cac42854b8f0e28b28d33b
José <jose@preault.com>
2023-03-29 10:36:21 +0200

    chore(front-end): changement recommandé par React

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOMClient from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 
@@ -10,26 +10,26 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 
 const queryClient = new QueryClient();
 
-ReactDOM.render(
-    <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<App />}>
-                        <Route index element={<Login page='login'/>} />
-                        <Route path='game1' element={<Login page='forgotPassword'/>} />
-                        <Route path='game2' element={<Login page='mobileLogin'/>} />
-                        <Route path='game3' element={<Login page='newPassword'/>} />
-                        <Route path='game4' element={<Login page='newPassword'/>} />
-                        <Route path='config' element={<Login page='newPassword'/>} />
-                        <Route path='finish' element={<Login page='newPassword'/>} />
-                        <Route path="*"element={<Page404/>} />
-                    </Route>
-                </Routes>
-            </Router>
-        </QueryClientProvider>
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+const container = document.getElementById('root');
+const root = ReactDOMClient.createRoot(container);
+
+root.render(<React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+        <Router>
+            <Routes>
+                <Route path="/" element={<App />}>
+                    <Route index element={<Login page='login'/>} />
+                    <Route path='game1' element={<Login page='forgotPassword'/>} />
+                    <Route path='game2' element={<Login page='mobileLogin'/>} />
+                    <Route path='game3' element={<Login page='newPassword'/>} />
+                    <Route path='game4' element={<Login page='newPassword'/>} />
+                    <Route path='config' element={<Login page='newPassword'/>} />
+                    <Route path='finish' element={<Login page='newPassword'/>} />
+                    <Route path="*"element={<Page404/>} />
+                </Route>
+            </Routes>
+        </Router>
+    </QueryClientProvider>
+</React.StrictMode>);
 
 /* <ReactQueryDevtools initialIsOpen={false} /> */
